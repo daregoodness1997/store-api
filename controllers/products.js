02bb15f5cf2f9dbc7f74455b8280ef4031d11926
daregoodness1997 <daregoodness@gmail.com>
2022-05-28 13:57:12 +0100

    get products and initial filters

--- controllers/products.js
+++ controllers/products.js
@@ -1,22 +1,30 @@
-const getAllProducts = (async = (req, res) => {
-  res.status(200).json({ msg: 'Gotten all products' });
-});
-const getAllStaticProducts = (async = (req, res) => {
-  throw new Error('testing async errors');
-});
+const Product = require('../models/Product');
 
-const getProduct = (async = (req, res) => {
+const getAllProducts = async (req, res) => {
+  const products = await Product.find({});
+  res.status(200).json({ nbHits: products.length, data: products });
+};
+const getAllStaticProducts = async (req, res) => {
+  const products = await Product.find({
+    featured: true,
+    name: 'vase table',
+  });
+  // throw new Error('testing async errors');
+  res.status(200).json({ nbHits: products.length, data: products });
+};
+
+const getProduct = async (req, res) => {
   res.status(200).json({ msg: 'Gotten a product' });
-});
-const createProduct = (async = (req, res) => {
+};
+const createProduct = async (req, res) => {
   res.status(200).json({ msg: 'Created a product' });
-});
-const deleteProduct = (async = (req, res) => {
+};
+const deleteProduct = async (req, res) => {
   res.status(200).json({ msg: 'Deleted a product' });
-});
-const updateProduct = (async = (req, res) => {
+};
+const updateProduct = async (req, res) => {
   res.status(200).json({ msg: 'Updated a product' });
-});
+};
 
 module.exports = {
   getAllProducts,
