db973700f4e0fc20220b9d49d0db9cdefafb5177
daregoodness1997 <daregoodness@gmail.com>
2022-06-01 16:03:01 +0100

    sort data

--- controllers/products.js
+++ controllers/products.js
@@ -1,20 +1,13 @@
 const Product = require('../models/Product');
 
 const getAllStaticProducts = async (req, res) => {
-  const search = 'ab';
-  const products = await Product.find({
-    // featured: true,
-    name: {
-      $regex: search,
-      $options: 'i',
-    },
-  });
+  const products = await Product.find({}).sort('-name price');
   // throw new Error('testing async errors');
   res.status(200).json({ nbHits: products.length, data: products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
 
   const queryObject = {};
   queryObject;
@@ -31,9 +24,17 @@ const getAllProducts = async (req, res) => {
     };
   }
 
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  // console.log(queryObject);
+  // const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
 
+  const products = await result;
   res.status(200).json({ nbHits: products.length, data: products });
 };
 
