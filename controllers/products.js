1a8c1720e7c47dd57684ac9b4168a312454268db
daregoodness1997 <daregoodness@gmail.com>
2022-06-13 12:26:38 +0100

    select method

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,13 @@
 const Product = require('../models/Product');
 
 const getAllStaticProducts = async (req, res) => {
-  const products = await Product.find({}).sort('-name price');
+  const products = await Product.find({}).select('name price');
   // throw new Error('testing async errors');
   res.status(200).json({ nbHits: products.length, data: products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryObject = {};
   queryObject;
@@ -27,11 +27,17 @@ const getAllProducts = async (req, res) => {
   // console.log(queryObject);
   // const products = await Product.find(queryObject);
   let result = Product.find(queryObject);
+  // sort
   if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('createdAt');
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
   }
 
   const products = await result;
