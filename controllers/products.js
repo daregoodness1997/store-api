68339832c9b372bf54253c4a34e78c69e31606b4
daregoodness1997 <daregoodness@gmail.com>
2022-06-26 17:16:58 +0100

    product controllerss updated

--- controllers/products.js
+++ controllers/products.js
@@ -78,16 +78,46 @@ const getAllProducts = async (req, res) => {
 };
 
 const getProduct = async (req, res) => {
-  res.status(200).json({ msg: 'Gotten a product' });
+  const {
+    params: { id: productId },
+  } = req;
+  const product = await Product.find({ productId });
+  res.status(200).json({ product });
 };
 const createProduct = async (req, res) => {
-  res.status(200).json({ msg: 'Created a product' });
+  const product = await Product.create(req.body);
+  res.status(200).json({ product });
 };
 const deleteProduct = async (req, res) => {
-  res.status(200).json({ msg: 'Deleted a product' });
+  const {
+    params: { id: productId },
+  } = req;
+
+  const product = await Product.findByIdAndDelete({ productId });
+  res.status(200).json({ msg: `Deleted a product of id ${productId}` });
 };
 const updateProduct = async (req, res) => {
-  res.status(200).json({ msg: 'Updated a product' });
+  const {
+    body: { name, price, featured, rating, company },
+    params: { id: productId },
+  } = req;
+
+  if (
+    name === '' ||
+    price === '' ||
+    featured === '' ||
+    rating === '' ||
+    company === ''
+  ) {
+    console.log('Fields should not be empty');
+  }
+  const product = await Product.findByIdAndUpdate({ productId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res
+    .status(200)
+    .json({ product, msg: `Updated a product of id ${productId}` });
 };
 
 module.exports = {
